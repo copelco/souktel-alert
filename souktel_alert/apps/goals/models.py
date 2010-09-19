bdcad7888992e7e04d3ae14cfe7b16206eaf069e
Colin Copeland <copelco@caktusgroup.com>
2010-09-19 17:45:19 -0400

    add basic schedule functionality

--- souktel_alert/apps/goals/models.py
+++ souktel_alert/apps/goals/models.py
@@ -6,9 +6,20 @@ from rapidsms.models import Connection, Contact
 
 
 class Goal(models.Model):
+    REPEAT_CHOICES = (
+        ('daily', 'Daily'),
+        ('weekly', 'Weekly'),
+        ('monthly', 'Monthly'),
+        ('yearly', 'Yearly'),
+    )
+    
     contact = models.ForeignKey(Contact, related_name='goals')
     date_created = models.DateTimeField()
-    date_last_notified = models.DateTimeField()
+    date_last_notified = models.DateTimeField(null=True, blank=True)
+    date_next_notified = models.DateTimeField(null=True, blank=True)
+    schedule_start_date = models.DateTimeField(null=True, blank=True)
+    schedule_repeat = models.CharField(max_length=16, choices=REPEAT_CHOICES,
+                                       blank=True)
     body = models.TextField()
     in_session = models.BooleanField(default=False)
     complete = models.BooleanField(default=False)
@@ -19,7 +30,6 @@ class Goal(models.Model):
     def save(self, **kwargs):
         if not self.pk:
             self.date_created = datetime.datetime.now()
-            self.date_last_notified = datetime.datetime.now()
         return super(Goal, self).save(**kwargs)
 
 
--- souktel_alert/apps/goals/views.py
+++ souktel_alert/apps/goals/views.py
@@ -1,8 +1,14 @@
+import datetime
+
 from django.template import RequestContext
 from django.shortcuts import redirect, render_to_response
 from django.db.models import Count, Max
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 from goals.models import Goal, Answer
+from goals.forms import ScheduleForm, GoalFormSet
 
 from group_messaging.decorators import contact_required
 
@@ -14,9 +20,30 @@ def summary(request):
         answers = answers.order_by('-count')[0]['count']
     except IndexError:
         answers = 0
+    goals = Goal.objects.order_by('-date_last_notified', '-date_created')
+    if request.POST:
+        form = ScheduleForm(request.POST)
+        formset = GoalFormSet(request.POST, queryset=goals)
+        if form.is_valid() and formset.is_valid():
+            frequency = form.cleaned_data['frequency']
+            start_date = form.cleaned_data['start_date']
+            enabled_goals = formset.save(commit=False)
+            now = datetime.datetime.now()
+            for goal in enabled_goals:
+                goal.schedule_start_date = start_date
+                goal.schedule_repeat = frequency
+                goal.save()
+            messages.info(request, 'Schedule saved successfully')
+            return HttpResponseRedirect(reverse('goal-summary'))
+    else:
+        form = ScheduleForm()
+        formset = GoalFormSet(queryset=goals)
+
     context = {
         'goals': Goal.objects.order_by('-date_last_notified'),
         'total_answers': xrange(answers),
+        'form': form,
+        'formset': formset,
     }
     return render_to_response('goals/summary.html', context,
                               context_instance=RequestContext(request))
