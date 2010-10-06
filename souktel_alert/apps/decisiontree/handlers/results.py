43847594978bbf277e967f0ad0b764f159c646db
Colin Copeland <copelco@caktusgroup.com>
2010-10-06 09:08:53 -0400

    add results handler

---
+++ souktel_alert/apps/decisiontree/handlers/results.py
@@ -0,0 +1,27 @@
+import datetime
+
+from rapidsms.models import Contact, Connection
+from rapidsms.messages import OutgoingMessage
+from rapidsms.contrib.scheduler.models import EventSchedule
+from rapidsms.contrib.handlers.handlers.keyword import KeywordHandler
+
+from decisiontree.models import Tree
+
+
+class ResultsHandler(KeywordHandler):
+
+    keyword = "results"
+
+    def help(self):
+        self.respond("Please enter a survey keyword")
+
+    def handle(self, text):
+        try:
+            tree = Tree.objects.get(trigger=text)
+        except Tree.DoesNotExist:
+            self.respond('Survey "%s" does not exist' % text)
+        if tree.summary:
+            self.respond(tree.summary)
+        else:
+            self.respond('No summary for "%s" survey' % text)
+        return True
