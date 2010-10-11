9bb44df79983ec5959de96fc7e74a2c9e28cc423
Colin Copeland <copelco@caktusgroup.com>
2010-10-11 12:19:43 -0400

    use system timezone

--- souktel_alert/settings.py
+++ souktel_alert/settings.py
@@ -21,7 +21,7 @@ PROJECT_NAME = 'twb-nigeria'
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-# TIME_ZONE = 'Africa/Lagos'
+TIME_ZONE = None
 
 
 # Language code for this installation. All choices can be found here:
